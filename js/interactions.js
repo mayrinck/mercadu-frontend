ee15ad110dd41ea1821e45b9c480e64ff8870c75
Renan Mayrinck <renan.mtk@gmail.com>
2019-04-03 19:27:35 -0300

    11

--- js/interactions.js
+++ js/interactions.js
@@ -1,5 +1,19 @@
+var CheckoutActive = 0;
+
 $(document).ready(function(){
   $(".menu").click(function(){
     $(".second-header").toggleClass("active");
+  });
+
+  $(".cart").click(function(){
+    $(".start-checkout").addClass("active");
+  });
+
+  $(".profile").click(function(){
+    $(".darkFrame").show();
+  });
+
+  $(".close-modal").click(function(){
+    $(".darkFrame").hide();
   });
 });
